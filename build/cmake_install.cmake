d5be94c7a692a9a344c94d767ee1d65086e0cff0
Xiaoda11 <1123297903@qq.com>
2022-08-11 16:12:32 +0800

    link

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/ljm/fangzhen_ws/build/gtest/cmake_install.cmake")
+  include("/home/ljm/fangzhen_ws/build/urdf01_rviz/cmake_install.cmake")
 
 endif()
 
